565edcfba3f999d7908311c3243f59a2295211be
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-01-31 12:29:32 -0800

    Clase EmpleadoTemporal agregada

--- src/lab2_programacion2q2/EmpleadoTemporal.java
+++ src/lab2_programacion2q2/EmpleadoTemporal.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab2_programacion2q2;
-
+import java.util.Calendar;
 /**
  *
  * @author Mario
  */
 public class EmpleadoTemporal {
+    private Calendar fechafinContrato;
     
 }
