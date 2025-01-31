dad34db4772fad88cd718760e82dd4867e295003
Mario Enrique  Vasquez Adriano <marioenriquevasquez4@gmail.com>
2025-01-31 12:55:28 -0800

    Agregue atributos,constructor y metodos,como el constructor EmpleadoTemporal,tamnbien un getfechafinContrato,tambien el setfechafinContrato,CalcularPago y mostrarInformacion

--- src/lab2_programacion2q2/EmpleadoTemporal.java
+++ src/lab2_programacion2q2/EmpleadoTemporal.java
@@ -8,7 +8,30 @@ import java.util.Calendar;
  *
  * @author Mario
  */
-public class EmpleadoTemporal {
+public class EmpleadoTemporal extends Empleado {
     private Calendar fechafinContrato;
+
+    public EmpleadoTemporal(Calendar fechafinContrato, int codigo, String nombre, double salarioBase, int horasTrabajadas) {
+        super(codigo, nombre, salarioBase, horasTrabajadas);
+        this.fechafinContrato = fechafinContrato;
+    }
     
+    public Calendar getfechafinContrato(){
+        return fechafinContrato;
+    }
+    
+    public void setfechafinContrato(Calendar fechafinContrato){
+        this.fechafinContrato = fechafinContrato;
+    }
+    
+    public double calcularPago() {
+        Calendar fechaActual = Calendar.getInstance();
+        if (fechaActual.before(fechafinContrato) || fechaActual.equals(fechafinContrato)) {
+            return (super.salarioBase / 160) * super.horasTrabajadas;
+        }
+        return 0.1;
+    }
+    public String mostrarInformacion(){
+        return super.mostrarInformacion() + " Fin del contrato " + fechafinContrato.getTime();
+    }
 }
