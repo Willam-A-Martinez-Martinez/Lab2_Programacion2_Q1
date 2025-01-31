39a18f1117aafbd801c57146333693bd55978e25
cbarahona29 <onnhyx1g@gmail.com>
2025-01-31 14:18:45 -0600

    Clase Empleado Ventas

---
+++ src/lab2_programacion2q2/EmpleadoVentas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2_programacion2q2;
+
+/**
+ *
+ * @author danilos
+ */
+public class EmpleadoVentas  {
+   double ventasMensuales[];
+   double comision;
+   
+}
