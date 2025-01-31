d223bf3225c6dd836060f23e9bd91283626e0c3b
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-01-31 14:13:51 -0600

    Atributos y metodos agregados

--- src/lab2_programacion2q2/Empleado.java
+++ src/lab2_programacion2q2/Empleado.java
@@ -4,10 +4,60 @@
  */
 package lab2_programacion2q2;
 
-/**
- *
- * @author DELL
+import java.util.Calendar;
+
+/*
+* Métodos Clave:
+
+* Registrar horas trabajadas: Permite actualizar el total de horas trabajadas por el empleado en el mes actual.
+
+* Cálculo de pago: Calcula el salario proporcional según las horas trabajadas, tomando un mes estándar de 160 horas como base.
+* Para empleados estándar, se aplica una deducción fija del 3.5%.
+
+* Mostrar información: Devuelve una cadena que incluye el código, el nombre y la fecha de contratación del empleado.
  */
+
+
 public class Empleado {
+    /**
+ * Propósito: Representar la base común para todos los empleados de la empresa, gestionando atributos generales y comportamientos comunes.
+Atributos:
+
+* Código único: Identifica de manera exclusiva a cada empleado.
+
+* Nombre: El nombre completo del empleado.
+
+* Fecha de contratación: Fecha en que el empleado ingresó a la empresa (se inicializa con la fecha actual). usar Calendar investigar.
+
+* Salario base: Representa la remuneración mensual estándar del empleado.
+
+* Horas trabajadas: Total de horas registradas por el empleado en el mes.
+*/
+    int codigo;
+    String nombre, fechaContratacion;
+    double salarioBase;
+    int horasTrabajadas;
+
+    public Empleado(int codigo, String nombre, double salarioBase, int horasTrabajadas) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasTrabajadas = horasTrabajadas;
+        fechaContratacion=String.valueOf(Calendar.getInstance());
+    }
     
+    public void registrarHoras(int horas){
+        horasTrabajadas+=horas;
+    }
+    
+    //Cálculo de pago: Calcula el salario proporcional según las horas trabajadas, tomando un mes estándar de 160 horas como base.
+    //Para empleados estándar, se aplica una deducción fija del 3.5%.
+    public double calculoPago(int horas){
+        return (horas/160)*salarioBase;
+    }    
+    
+//    Mostrar información: Devuelve una cadena que incluye el código, el nombre y la fecha de contratación del empleado.
+    public String mostrarInformacion(){
+        return "Nombre del empleado: "+nombre+"\nCodigo del empleado: "+codigo+"\nFecha de contratacion: "+fechaContratacion;
+    }
 }
